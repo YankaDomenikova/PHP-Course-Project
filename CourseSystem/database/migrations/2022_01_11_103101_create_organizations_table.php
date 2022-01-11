86af95b35d475a7ed1c0cb13212751a5ef3a4db8
Yanka Domenikova <yankadomenikova@gmail.com>
2022-01-11 12:34:50 +0200

    Add models and controllers for course, teacher and organization

---
+++ CourseSystem/database/migrations/2022_01_11_103101_create_organizations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("organization_name");
+            $table->string("city");
+            $table->timestamps();
+        });
+
+        Schema::table('courses', function($table)
+        {
+            $table->bigInteger('organization_id')->unsigned();
+            $table->foreign('organization_id')->references('id')->on('organizations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
